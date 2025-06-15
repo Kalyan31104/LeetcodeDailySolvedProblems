63c236837a639867205fdfa35ecd9ade559f9c07
Kalyan31104 <vuppureddykalyan@gmail.com>
2025-06-15 13:07:40 +0530

    Create dailysolvedproblem5.java

---
+++ dailysolvedproblem5.java
@@ -0,0 +1,52 @@
+/*
+3442. Maximum Difference Between Even and Odd Frequency I
+
+You are given a string s consisting of lowercase English letters.
+Your task is to find the maximum difference diff = freq(a1) - freq(a2) between the frequency of characters a1 and a2 in the string such that:
+a1 has an odd frequency in the string.
+a2 has an even frequency in the string.
+Return this maximum difference.
+
+Example 1:
+Input: s = "aaaaabbc"
+Output: 3
+Explanation:
+The character 'a' has an odd frequency of 5, and 'b' has an even frequency of 2.
+The maximum difference is 5 - 2 = 3.
+
+Example 2:
+Input: s = "abcabcab"
+Output: 1
+Explanation:
+The character 'a' has an odd frequency of 3, and 'c' has an even frequency of 2.
+The maximum difference is 3 - 2 = 1.
+ Constraints:
+  3 <= s.length <= 100
+  s consists only of lowercase English letters.
+  s contains at least one character with an odd frequency and one with an even frequency. */
+
+//code:
+
+class Solution {
+    public int maxDifference(String s) {
+        int arr[]=new int[26];
+        int evenfreq=s.length(),oddfreq=0;
+        for(int i=0;i<s.length();i++){
+            int index=s.charAt(i)-'a';
+            arr[index]++;
+        }
+        for(int i=0;i<26;i++){
+            if((arr[i]!=0)&&arr[i]%2==0){
+                if(arr[i]<evenfreq){
+                    evenfreq=arr[i];
+                }
+            }
+            else if((arr[i]!=0)&&arr[i]%2!=0){
+                if(arr[i]>oddfreq){
+                    oddfreq=arr[i];
+                }
+            }
+        }
+        return (oddfreq-evenfreq);
+    }
+}
