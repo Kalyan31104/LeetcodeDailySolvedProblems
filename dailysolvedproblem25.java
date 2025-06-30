589ad0ea6f3ea8c2a1f417a21ff58fe526415ab1
Kalyan31104 <vuppureddykalyan@gmail.com>
2025-06-30 18:15:50 +0530

    Create dailysolvedproblem25.java

---
+++ dailysolvedproblem25.java
@@ -0,0 +1,62 @@
+/*
+594. Longest Harmonious Subsequence
+We define a harmonious array as an array where the difference between its maximum value and its minimum value is exactly 1.
+Given an integer array nums, return the length of its longest harmonious subsequence among all its possible subsequences.
+
+Example 1:
+Input: nums = [1,3,2,2,5,2,3,7]
+Output: 5
+Explanation:
+The longest harmonious subsequence is [3,2,2,2,3].
+
+Example 2:
+Input: nums = [1,2,3,4]
+Output: 2
+Explanation:
+The longest harmonious subsequences are [1,2], [2,3], and [3,4], all of which have a length of 2.
+
+Example 3:
+Input: nums = [1,1,1,1]
+Output: 0
+Explanation:
+No harmonic subsequence exists.
+
+Constraints:
+1 <= nums.length <= 2 * 104
+-109 <= nums[i] <= 109
+*/
+
+//solution
+class Solution {
+    public int findLHS(int[] nums) {
+        Arrays.sort(nums);
+        int count=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums.length-i<=count)
+            {
+                break;
+            }
+            int a=max(nums,i);
+            if(a>count){
+                count=a;
+            }
+        }
+        return count;
+        
+    }
+    static int max(int arr[],int start){
+        boolean flag=false;
+        int end=arr.length-1;
+        while(start<end){
+            if(arr[end]-arr[start]==1){
+                flag=true;
+                break;
+            }
+            end--;
+        }
+        if(flag){
+            return end-start+1;
+        }
+        return 0;       
+  }
+}
